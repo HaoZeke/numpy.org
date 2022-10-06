a1e096a528f59077f4827dff577989a00a037f40
Sourcery AI <>
2022-10-06 16:16:26 +0000

    'Refactored by Sourcery'

--- gen_config.py
+++ gen_config.py
@@ -8,15 +8,14 @@ with open('config.yaml.in', 'r', encoding='utf-8') as templ:
 pattern = re.compile('< content\/\w\w\/\w*.yaml >')
 with open('config.yaml', 'w', encoding='utf-8') as f:
     for line in lines:
-        match = pattern.search(line)
-        if match:
+        if match := pattern.search(line):
             with open(match.group()[2:-2], 'r', encoding='utf-8') as f2:
-                for f2_line in f2.readlines():
+                for f2_line in f2:
                     # indent to get correct yaml formatting
-                    f.write('    ' + f2_line)
+                    f.write(f'    {f2_line}')
         elif line.startswith('disableLanguages'):
             if os.environ.get('NUMPYORG_WITH_TRANSLATIONS'):
-                line = "#" + line
+                line = f"#{line}"
 
             f.write(line)
         else:
